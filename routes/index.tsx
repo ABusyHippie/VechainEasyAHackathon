fc6a1107b130c1d6663b3a50b9c2ed1ccfb6766a
BrotherBaner <25873736+ABusyHippie@users.noreply.github.com>
2024-06-29 13:52:50 -0700

    newest pushes

--- routes/index.tsx
+++ routes/index.tsx
@@ -14,8 +14,8 @@ export default function Home() {
       <section id="home">
         <img src="/img/water.png" alt="background" id="background"></img>
         <h2 id="text">
-          <span>Compete With Your Neighbors</span>
-          <br />Save The Planet
+          <span>Reduce Your Utility Usage</span>
+          <br />Make B3TR Token
         </h2>
         <a href="#waterCycle" id="btn">Connect</a>
         
@@ -23,52 +23,35 @@ export default function Home() {
 
       <section id="waterCycle" >
         <img src="/img/water.png" alt="background" id="background"></img>
-        <h2 id="text">
-          <span>Compete With Your Neighbors</span>
-          <br />Save The Planet
+        <h2 id="text-water">
+          <br />Use Less Water <br />
+          <span>Daily Reminders of sustainable habits</span> <br />
+          <span>Collect Rainwater Challenge 💧</span> <br />
+          <span>Grow Your Own Veggie Garden Multiplier 🥕</span>
         </h2>
-        <p>
-          1- Evaporation: Water from the oceans, rivers, lakes, and puddles gets heated by the sun and turns into vapor. Imagine the water turning into "invisible clouds" that go up into the sky. <br />
-          <br />2- Condensation: The water vapor cools in the sky and turns back into tiny water droplets. This forms clouds, which look like fluffy cotton in the sky. <br />
-          <br />3- Precipitation: When the water droplets in the clouds come together and get very heavy, they fall to the Earth as rain, snow, or hail. It's like the clouds are crying.
-        </p>
-        <a href="#waterCycle2" id="btn">Water Cycle 2</a>
-      </section>
-  
-      <section id="whatIsClimateChange" class="sec">
-        <a href="#waterCycle2" id="btn">Water Cycle 2</a>
-        <h2>What Is Climate Change?</h2>
-        <p>
-          Climate change describes a change in the average conditions — such as temperature and rainfall — in a region over a long period of time. NASA scientists have observed Earth’s surface is warming, and many of the warmest years on record have happened in the past 20 years.
-        </p>
-        <a href="#climateChange" id="btn">Climate Change</a>
+        <a href="#waterCycle2" id="btn">Submit Your Water Bill Now</a>
       </section>
       
-      <section id="climateChange" class="sec">
-        <a href="#whatIsClimateChange" id="btn">What Is Climate Change?</a>
-        <h2>Climate Change</h2>
-        <p>
-          The Earth's weather has changed a little bit over a very long time. But now, it's getting warmer faster than it has in a really, really long time, like more than 10,000 years!
-        </p>
-        <a href="#humanInfluence" id="btn">Human Influence</a>
+      <section id="waterCycle" >
+        <img src="/img/water.png" alt="background" id="background"></img>
+        <h2 id="text-electric">
+          <br />Use Less Electricity <br />
+          <span>Try High Efficiency Bulbs 💡</span> <br />
+          <span>Get a Bonus for Giving Back to the Grid ⚡️</span> <br />
+          <span>Save on Your Bill by Watching With Friends 📺</span>
+        </h2>
+        <a href="#waterCycle2" id="btn">Submit Your Water Bill Now</a>
       </section>
 
-      <section id="humanInfluence" class="sec">
-        <a href="#climateChange" id="btn">Climate Change</a>
-        <h2>Human Influence</h2>
-        <p>
-          Scientists have been studying this for a while, and they're sure that people are making the Earth warmer. They've used all sorts of tools, like looking at really old ice, rocks, and tree rings, as well as super high-tech things like satellites and special machines.
-        </p>
-        <a href="#waysOfKnowing" id="btn">Ways of Knowing</a>
-      </section>
-
-      <section id="waysOfKnowing" class="sec">
-        <a href="#humanInfluence" id="btn">Human Influence</a>
-        <h2>Ways of Knowing</h2>
-        <p>
-          Scientists use different tools and sources of information to understand how the climate is changing. Some of these are very old, like pieces of ice, rocks, and tree rings. They also use modern technology, like satellites and special machines.
-        </p>
-        <a href="#evidenceOfWarning" id="btn">Evidence of Warming</a>
+      <section id="waterCycle" >
+        <img src="/img/water.png" alt="background" id="background"></img>
+        <h2 id="text-water">
+          <br />Use Less Gas <br />
+          <span>Recycle Your Cooking Oil with  <a href="https://oily.uco.network/home">Oily</a></span> <br />
+          <span>Collect Rainwater Challenge 💧</span> <br />
+          <span>Grow Your Own Veggie Garden Multiplier 🥕</span>
+        </h2>
+        <a href="#waterCycle2" id="btn">Submit Your Water Bill Now</a>
       </section>
 
       <section id="evidenceOfWarning" class="sec">
@@ -87,7 +70,6 @@ export default function Home() {
             alt="Made with Fresh"
           />
         </a>
-        <p>Developed by <a href="https://rak1u.deno.dev/">Rak1u</a></p>
       </footer>
     </div>
   );
