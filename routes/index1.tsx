6ee142d565058d7c1fe492defa17386cd0ea775b
BrotherBaner <25873736+ABusyHippie@users.noreply.github.com>
2024-06-29 17:53:25 -0700

    add corporate assets

---
+++ routes/index1.tsx
@@ -0,0 +1,75 @@
+import { useSignal } from "@preact/signals";
+import { useEffect } from "preact/hooks";
+
+export default function Home() {
+  return (
+    <div>
+      <header id="header">
+        <a href="#" class="logo">Power Up!</a>
+        {/* <ul>
+          <li><p id="text">Reduce your utilities use, make B3TR!</p></li>
+        </ul> */}
+      </header>
+
+      <section id="home">
+        <img src="/img/grasslogos.png" alt="background" id="background"></img>
+        <h2 id="text">
+          <span>Slash Your Utility Bills, Beat Your Neighbors</span><br />
+          <br />Make Your Hood B3TR
+        </h2>
+        <a href="#waterCycle" id="btn">Connect</a>
+      </section>
+
+      <section id="waterCycle" >
+        <img src="/img/water.png" alt="background" id="background"></img>
+        <h2 id="text-water">
+          <br />Use Less Water <br />
+          <span>Daily Reminders of sustainable habits</span> <br />
+          <span>Collect Rainwater Challenge 💧</span> <br />
+          <span>Grow Your Own Veggie Garden Multiplier 🥕</span>
+        </h2>
+        <a href="#waterCycle2" id="btn">Submit Your Water Bill Now</a>
+      </section>
+      
+      <section id="waterCycle" >
+        <img src="/img/electric.png" alt="background" id="background"></img>
+        <h2 id="text-electric">
+          <br />Use Less Electricity <br />
+          <span>Try High Efficiency Bulbs 💡</span> <br />
+          <span>Get a Bonus for Giving Back to the Grid ⚡️</span> <br />
+          <span>Save on Your Bill by Watching With Friends 📺</span>
+        </h2>
+        <a href="#waterCycle2" id="btn-lit">Submit Your Electric Bill Now</a>
+      </section>
+
+      <section id="waterCycle" >
+        <img src="/img/fire.png" alt="background" id="background"></img>
+        <h2 id="text-gas">
+          <br />Use Less Gas <br />
+          <span>Recycle Your Cooking Oil with  <a href="https://oily.uco.network/home">Oily</a></span> <br />
+          <span>Try the Cold Shower Challenge 🥶</span> <br />
+          <span>Smart Heating Only The Rooms Your In 🧠</span>
+        </h2>
+        <a href="#waterCycle2" id="btn-gas">Submit Your Gas Bill Now</a>
+      </section>
+
+      <section id="evidenceOfWarning" class="sec">
+        <a href="#waysOfKnowing" id="btn">Ways of Knowing</a>
+        <h2>Evidence of Warming</h2>
+        <p>
+          There are many signs that the Earth is getting warmer, from the rise in temperature all around the world to the melting of large ice sheets.
+        </p>
+      </section>
+      
+      <footer id="footer">
+        <a href="https://fresh.deno.dev" id="deno-btn">
+          <img
+            id="deno"
+            src="https://fresh.deno.dev/fresh-badge.svg"
+            alt="Made with Fresh"
+          />
+        </a>
+      </footer>
+    </div>
+  );
+}
